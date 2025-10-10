249d12e5e0c95f17f71be061e6caa116ef7e525d
Muhammed Ibrahem (Sokkar) <muhammedibrahem001@gmail.com>
2025-10-10 22:01:32 +0300

    feat: add currency error handling

--- src/Domains/Currency/Repository/CurrencyRepository.php
+++ src/Domains/Currency/Repository/CurrencyRepository.php
@@ -4,20 +4,27 @@ declare(strict_types=1);
 
 namespace App\Domains\Currency\Repository;
 
+use PDOException;
+
 use App\Core\Repository\BaseRepository;
+use App\Exceptions\DatabaseException;
 
 class CurrencyRepository extends BaseRepository
 {
     public function findByIds(array $ids): array
     {
-        $placeholder = \join(",", \array_pad([], \count($ids), "?"));
+        try {
+            $placeholder = \join(",", \array_pad([], \count($ids), "?"));
 
-        $query = "SELECT * FROM currency WHERE id IN ({$placeholder})";
+            $query = "SELECT * FROM currency WHERE id IN ({$placeholder})";
 
-        $stmt = $this->connection->prepare($query);
+            $stmt = $this->connection->prepare($query);
 
-        $stmt->execute($ids);
+            $stmt->execute($ids);
 
-        return $stmt->fetchAll();
+            return $stmt->fetchAll();
+        } catch (PDOException $e) {
+            throw new DatabaseException();
+        }
     }
 }
