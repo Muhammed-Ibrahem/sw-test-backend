2eef5a771a1bbe7c23c6c7a8b36413016af8b3ec
Muhammed Ibrahem (Sokkar) <muhammedibrahem001@gmail.com>
2025-10-10 22:02:54 +0300

    feat: add prices error handling

--- src/Domains/Price/Repository/PriceRepository.php
+++ src/Domains/Price/Repository/PriceRepository.php
@@ -4,22 +4,29 @@ declare(strict_types=1);
 
 namespace App\Domains\Price\Repository;
 
+use PDOException;
+
 use App\Core\Repository\BaseRepository;
+use App\Exceptions\DatabaseException;
 
 class PriceRepository extends BaseRepository
 {
     public function findPriceByProductIds(array $ids): array
     {
-        if (empty($ids)) return [];
+        try {
+            if (empty($ids)) return [];
 
-        $placeholder = \join(",", \array_pad([], \count($ids), "?"));
+            $placeholder = \join(",", \array_pad([], \count($ids), "?"));
 
-        $query = "SELECT * FROM price WHERE product_id IN ({$placeholder})";
+            $query = "SELECT * FROM price WHERE product_id IN ({$placeholder})";
 
-        $stmt = $this->connection->prepare($query);
+            $stmt = $this->connection->prepare($query);
 
-        $stmt->execute($ids);
+            $stmt->execute($ids);
 
-        return $stmt->fetchAll();
+            return $stmt->fetchAll();
+        } catch (PDOException $e) {
+            throw new DatabaseException();
+        }
     }
 }
